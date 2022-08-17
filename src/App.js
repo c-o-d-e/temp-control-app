d284cb1abc35f9c378f9eb5d11d16c0cd9167315
c-o-d-e <swapyuk@gmail.com>
2022-08-17 12:45:55 +0100

    project objectives achieved

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { useState } from "react";
 
 function App() {
+    const [initialTemp, newTemp] = useState(10);
+    const [initClass, newClass] = useState("circle-bg-cool");
+
+    const addTemp = () => {
+        const currentTemp = initialTemp + 1;
+        if (currentTemp > 30) return;
+        if (currentTemp >= 15) {
+            newClass("circle-bg-hot");
+        }
+        newTemp(currentTemp);
+    };
+    const removeTemp = () => {
+        const currentTemp = initialTemp - 1;
+        if (currentTemp < 0) return;
+        if (currentTemp < 15) {
+            newClass("circle-bg-cool");
+        }
+        newTemp(currentTemp);
+    };
+
     return (
         <main>
-            <div className="circle">
-                <h1>10°C</h1>
+            <div className={`circle ${initClass}`}>
+                <h1>{initialTemp}°C</h1>
             </div>
             <div className="button-wrapper">
-                <button className="buttons">+</button>
-                <button className="buttons">-</button>
+                <button className="buttons" onClick={addTemp}>
+                    +
+                </button>
+                <button className="buttons" onClick={removeTemp}>
+                    -
+                </button>
             </div>
         </main>
     );
